7c72960efa316edb94d04e2aba4a1a395dd94eaf
Katia Hancock <katianahancock98@gmail.com>
2018-07-11 15:58:14 -0400

    added map

--- script.js
+++ script.js
@@ -4,7 +4,6 @@ startButtonOff.addEventListener("click", () => {
 
     document.getElementById("start").disabled = true;
     document.getElementById("quit").disabled = false;
-    console.log("It worked!");
 
 })
 
@@ -17,3 +16,17 @@ startButtonRefresh.addEventListener("click", () => {
 
 })
 
+var map = L.map('map').setView([51.505, -0.09], 13);
+
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(map);
+
+L.marker([51.5, -0.09]).addTo(map)
+    .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
+    .openPopup();
+
+// L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}',
+//   {
+//     attribution: 'Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community'
+//   }).addTo(mymap);
