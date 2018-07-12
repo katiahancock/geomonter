6024d1e53eb4a9f0a1f5851014d09af89797b1a1
Katia Hancock <katianahancock98@gmail.com>
2018-07-12 11:33:26 -0400

    no more drag

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// The start button is disabled when you click on it!
 let startButtonOff = document.getElementById("start");
 
 startButtonOff.addEventListener("click", () => {
@@ -7,6 +8,7 @@ startButtonOff.addEventListener("click", () => {
 
 })
 
+// When you click the "I give up" button, the start button is enabled again!
 let startButtonRefresh = document.getElementById("quit");
 
 startButtonRefresh.addEventListener("click", () => {
@@ -16,25 +18,36 @@ startButtonRefresh.addEventListener("click", () => {
 
 })
 
+// The polygon shape that defines Vermont, but leaves out a couple of NH-border towns.
 let vermontPolygon = L.geoJSON(border_data);
 
 let vermontBounds = vermontPolygon.getBounds();
 
 let vermontCenter = vermontBounds.getCenter();
 
-var map = L.map("map").setView(vermontCenter, 7);
+// var map = L.map("map").setView(vermontCenter, 16.5); - was supposed to set the view so I can see the whole polygon, but not necessary for game overall.
 
+// Sets the map layer - specifically satellite, in this case.
 L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}',
   {
+
     attribution: 'Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community'
-  }).addTo(map);
 
-// L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-//     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-// }).addTo(map);
+}).addTo(map);
 
-// L.marker([51.5, -0.09]).addTo(map)
-//     .bindPopup('A pretty CSS3 popup.<br> Easily customizable.');
-
+// Adding the Vermont polygon to the map itself!
 vermontPolygon.addTo(map);
 
+let boundingBox = {
+    maxLon: -73.3654,
+    minLon: -71.5489,
+    maxLat: 45.0065,
+    minLat: 42.7395
+};
+
+// Set map zoom - to give them hope, muahahahaha
+map.setMaxZoom(18);
+map.setMinZoom(16);
+
+// You wanted dragging? You did not get it. You poor bastard.
+map.dragging.disable();
